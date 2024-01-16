a12f5c3ada183833eb485eee52035dc70429bc65
Scara-08 <kackinbaevaaana@gmail.com>
2024-01-16 16:28:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
-function App() {
+const StopwatchApp = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState([]);
+  const intervalRef = useRef();
+
+  const startStopwatch = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+    } else {
+      const startTime = Date.now() - time;
+      intervalRef.current = setInterval(() => {
+        setTime(Date.now() - startTime);
+      }, 100);
+    }
+    setIsRunning(!isRunning);
+  };
+
+  const resetStopwatch = () => {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+    setTime(0);
+    setLaps([]);
+  };
+
+  const addLap = () => {
+    setLaps([...laps, time]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Timer</h1>
+      <p>{formatTime(time)}</p>
+      <button onClick={startStopwatch}>{isRunning ? 'Stop' : 'Start'}</button>
+      <button onClick={resetStopwatch}>Clear</button>
+      <button onClick={addLap}>Result</button>
+      <ul>
+        {laps.map((lap, index) => (
+          <li key={index}>{`Lap ${index + 1}: ${formatTime(lap)}`}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+const formatTime = (time) => {
+  const milliseconds = (`00${time % 1000}`).slice(-3);
+  const seconds = (`0${Math.floor(time / 1000) % 60}`).slice(-2);
+  const minutes = (`0${Math.floor(time / 60000) % 60}`).slice(-2);
+  return `${minutes}:${seconds}.${milliseconds}`;
+};
+
+export default StopwatchApp;
+
+
+
+// function App() {
+//   return ()
+    
+
+ 
+// }
+
+// export default App;
